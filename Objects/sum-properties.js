62beec424ea6d0be6b9117b5d752094e129f4569
bruno <bpires.siqueira@gmail.com>
2023-08-07 09:54:18 -0300

    sum properties...

---
+++ Objects/sum-properties.js
@@ -0,0 +1,10 @@
+// • Crie um objeto com diversas propriedades, e o valor delas precisa ser um número
+// • Depois crie uma função que recebe este objeto, e entrega a soma de todas as propriedades como retorno
+// • Exiba os resultados no console ou na tela (HTML)
+
+const properties = {
+  valor: 4,
+  valor2: 21,
+  valor3: 44,
+  valor4: 9,
+};
